dc8aef19bb7391faef220b675a11f105793654c6
ty park <patayokr@naver.com>
2024-07-02 21:24:38 +0900

    add SceneManager

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class CrashDetector : MonoBehaviour
     {
         if (other.CompareTag("Ground"))
         {
-            Debug.Log("Crashed!!");
+            SceneManager.LoadScene(0);
         }
     }
 }
